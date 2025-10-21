95024153d363b4d2b1a627aa25981653ab8ecfd6
alumno <alumno@MACB1.local>
2025-10-21 09:50:00 -0600

    el fire

---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdEX9M-hFv4sy_4fJh2Si5roa_Ou93ErE",
+  authDomain: "trabajo26-c282c.firebaseapp.com",
+  projectId: "trabajo26-c282c",
+  storageBucket: "trabajo26-c282c.firebasestorage.app",
+  messagingSenderId: "10483374531",
+  appId: "1:10483374531:web:5f73f36e569b73c5aec576"
+};
